2172dd1d3eaea313c4fa6c3966d9a5bf01828772
Gedeon-programmer <twizerimanagedeon273@gmail.com>
2024-01-24 16:03:37 +0200

    about us

---
+++ javascript/text.js
@@ -0,0 +1,25 @@
+
+function hiderword(){
+    const showers= document.querySelector(".hid-content").style.display="block"
+}
+
+function downloadPDF() {
+    // Replace 'path/to/your/file.pdf' with the actual path to your PDF file
+    var pdfLink = 'path/to/your/file.pdf';
+    
+    // Create a temporary link element
+    var link = document.createElement('a');
+
+    // Set the download attribute and file path
+    link.download = 'downloaded_file.pdf';
+    link.href = pdfLink;
+
+    // Append the link to the document
+    document.body.appendChild(link);
+
+    // Trigger a click on the link element
+    link.click();
+
+    // Remove the link element from the document
+    document.body.removeChild(link);
+}
